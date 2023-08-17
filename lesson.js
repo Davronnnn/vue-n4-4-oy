8f45b5473cea020277a2a7cc992ff32fd574dce4
Davronnnn <kifleader@gmail.com>
2023-08-17 18:38:37 +0500

    array methods

---
+++ lesson.js
@@ -0,0 +1,7 @@
+const array = ["asd","asdasd","123123a"];
+const array2 = ["asd","asdasd","123123a"];
+
+const text = "lorem nimadir kas asasd asdas";
+
+const mergedArray = array.concat(array2)
+console.log(mergedArray)
