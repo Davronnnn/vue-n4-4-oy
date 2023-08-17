5857664dc8349f49bf9cb96fc90a5be3d7784aea
Davronnnn <kifleader@gmail.com>
2023-08-17 20:41:53 +0500

    last

--- lesson.js
+++ lesson.js
@@ -1,7 +1,12 @@
-const array = ["asd","asdasd","123123a"];
-const array2 = ["asd","asdasd","123123a"];
+const array = [3,4,1,5];
 
-const text = "lorem nimadir kas asasd asdas";
+// const array2 = ["asd","asdasd","123123a"];
 
-const mergedArray = array.concat(array2)
-console.log(mergedArray)
+// const text = "lorem nimadir kas asasd asdas";
+
+// const mergedArray = array.concat(array2)
+// console.log(mergedArray)
+
+console.log(array.reduce((a,b) => {
+    return a + b
+}));
